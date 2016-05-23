dbdeabae5445f08ce4f9aa4f4220c37953369c62
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-23 15:39:16 +0200

    add prop onClickSave

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
 import BookList from '../containers/book_list';
+import InputBook from './input_book';
 
 export default class App extends Component {
   render() {
     return (
-      <BookList />
+      <div>
+	      <InputBook onClickSave={this.props.onDispatchAction} />
+	      <BookList />
+      </div>
     );
   }
 }
