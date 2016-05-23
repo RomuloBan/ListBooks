ac6d903a664848f03817f432ab448a014b055b7d
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-23 15:40:11 +0200

    add dispatchAction function and store variable

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+var store = createStore(reducers);
+
+
+const dispatchAction = (action) => {
+	store.dispatch(action);
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <App onDispatchAction={dispatchAction}/>
   </Provider>
   , document.querySelector('.container'));
