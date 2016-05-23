68060ba71ad2656605e66d17fa70eceab1680aec
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-23 11:17:23 +0200

    add BookReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  bookd: BooksReducer
+  books: BooksReducer
 });
 
 export default rootReducer;
