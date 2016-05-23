4ae51bb1268424abf5c599b1f344c1dd1f3b086e
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-23 15:41:32 +0200

    add switch statement width add_book case

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,14 @@
-export default function () {
-	return [
-		{title: 'JavaScript: The Good Parts'},
-		{title: 'Silmarillon'},
-		{title: 'Snowcrash'},
-		{title: 'Retromania'},
-		{title: 'Interactive Programming'}
-	];
+export default function (state = [], action) {
+
+	switch(action.type) {
+
+		case 'ADD_BOOK':
+			return [...state, Object.assign({}, { title: action.title })];
+		break;
+
+		default:
+
+		return state;
+	}
+
 }
