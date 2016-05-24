f5b3fa6fbbcd48c51b08e15e6753ffb1e8975d0f
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-24 18:34:08 +0200

    add remove_book case to books_reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,8 +3,18 @@ export default function (state = [], action) {
 	switch(action.type) {
 
 		case 'ADD_BOOK':
-			return [...state, Object.assign({}, { title: action.title })];
-		break;
+		return [...state, book(undefined, action)];
+
+
+		case 'SELECT_BOOK':			
+		return state.map(t => 
+                book(t, action)
+            );
+
+		case 'REMOVE_BOOK':			
+		return state.filter(t => 
+                book(t, action)
+            );
 
 		default:
 
@@ -12,3 +22,36 @@ export default function (state = [], action) {
 	}
 
 }
+
+let incrementalId = 0;
+
+
+
+const book = (state, action) => {
+    switch (action.type) {
+        case 'ADD_BOOK':
+            return {
+                id: incrementalId++,
+                title: action.title,
+                selected: false
+            };
+        case 'SELECT_BOOK':
+            if (state.id !== action.id) {
+                return state;
+            }
+
+            return {
+                ...state,
+                selected: !state.selected
+            };
+         case 'REMOVE_BOOK':
+            if (state.id !== action.id) {
+                return true;
+            }
+            return false;
+
+           
+        default:
+            return state;
+    }
+};
