6391fa88d1b5b29a365ad45104a45bfbca422a6c
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-24 18:30:34 +0200

    add btn-default class and action constant

--- src/components/input_book.js
+++ src/components/input_book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import actions from '../actions/index';
 
 
 const InputBook = ({ dispatch }) => {
@@ -7,15 +8,15 @@ const InputBook = ({ dispatch }) => {
 	let input;
 
 	const clickHandler = () => {
-		if(input.value) dispatch({ type: 'ADD_BOOK', title: input.value });
+		if(input.value) dispatch({ type: actions.ADD_BOOK, title: input.value });
 		
 		input.value = '';
 	};
 
 	return(
-		<div>
+		<div className="input-book">
 			<input ref={(ref) => { input = ref }} type="text" />
-			<button onClick={clickHandler} >Save</button>
+			<button type="button" className="btn btn-default" onClick={clickHandler} >Save</button>
 		</div>
 	);
 }
