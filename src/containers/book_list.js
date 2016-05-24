bd8e18eebaf2dea26bf4b385cc82a507e3def645
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-24 18:32:36 +0200

    add handler functions to books li items

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -3,14 +3,18 @@ import { connect } from 'react-redux';
 
 class BookList extends Component {
 
+
+
 	renderList() {
 		return this.props.books.map( book => {
-				return <li  key={book.title} >{book.title}</li>
+
+				return (<li className="list-group-item" style={{'backgroundColor': book.selected ? 'orange' : ''}} key={book.title} ><div className="book-title" onClick={() => this.props.onBookSelected(book)}>{book.title}</div><div onClick={() => {this.props.onBookRemoved(book)}} className="glyphicon glyphicon-remove pull-right remove-icon"></div></li>);
+					   
 		});
 	}
 
 	render() {
-		return (<ul>
+		return (<ul className="list-group book-list">
 				{this.renderList()}
 				</ul>);		
 	}
@@ -20,10 +24,25 @@ class BookList extends Component {
 const mapStateToProps = (state) => {
 	return {
 		books: state.books
-	};
-	
+	};	
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onBookSelected: (book) => { 
+			dispatch({ type: 'SELECT_BOOK',
+						 id: book.id,
+					  title: book.title
+					});
+		},
+		onBookRemoved: (book) => { 
+			dispatch({ type: 'REMOVE_BOOK',
+						 id: book.id
+					});
+		}
+	};	
 }
 
 
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
